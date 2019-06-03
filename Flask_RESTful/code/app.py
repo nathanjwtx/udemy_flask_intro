b19dcebbd06887bfdbe868633733097314b0b4e4
Nathan Waterman <nathanjwtx@nathanjwtx.io>
2019-06-03 06:52:23 -0500

    Flask_RESTful: added reqparse to verify and check payload data

--- Flask_RESTful/code/app.py
+++ Flask_RESTful/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -18,6 +18,12 @@ items = [{
 
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("price",
+                        type=float,
+                        required=True,
+                        help="Cannot be blank")
+
     @jwt_required()
     def get(self, item_name):
         item = list(filter(lambda x: x["name"].lower() ==
@@ -26,9 +32,13 @@ class Item(Resource):
         return {"item": item[:1]}, 200 if item else 404
 
     def post(self, item_name):
+        # checking for errors first before working on the payload
         if next(filter(lambda x: x["name"].lower() ==
                        item_name.lower(), items), None):
             return {"Message": f"{item_name} already exists"}, 400
+
+        data = Item.parser.parse_args()
+
         request_data = request.get_json()
         item = {"name": item_name.capitalize(), "price": request_data["price"]}
         items.append(item)
@@ -41,7 +51,8 @@ class Item(Resource):
         return {"message": f"{item_name} deleted"}
 
     def put(self, item_name):
-        data = request.get_json()
+        # data = request.get_json() # replaced by parser code above
+        data = parser.parse_args()
         item = next(filter(lambda x: x["name"].lower() == item_name.lower(),
                     items), None)
         if item is None:
